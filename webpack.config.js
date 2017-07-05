a999704c6000e1aeb38518d56df07a73583f44ff
NoBey <NoBey@nobey.cn>
2017-07-05 19:22:41 +0800

    add easy

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+       filename: 'index.js'
+  },
+  externals: {
+    // 'React': 'react'
+  },
+  module: {
+    rules: [{
+          test: /\.js(.*)$/,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+          options: {
+            presets: ['latest', 'stage-0', 'react']
+          }
+        }]
+  },
+  devServer: {
+  contentBase: path.join(__dirname, "build"),
+  compress: true,
+  port: 9000
+}
+}
